4062cbea11ef2a43a6be3485134e25f632822208
Julius Thaller <thallerjulius@gmail.com>
2023-12-14 16:34:43 +0100

    Initial Commit

---
+++ ccbuild.cc
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <cstring>
+#include <regex>
+#include <algorithm>
+
+std::vector<std::string> checkFile(std::string fileName){
+    std::ifstream file(fileName);
+    std::vector<std::string> dependencies;
+    std::string match = "#include \"";
+
+    if(file.is_open()){
+        std::string line;
+
+        // Read the file
+        while(std::getline(file, line)){
+            // Go through the line and find all occurences of #include 
+            std::regex pattern("#includ.*\"(.*)\"");
+            std::smatch match;
+            if (std::regex_match(line, match, pattern)) {
+                if (match.size() > 1) {
+                    if(std::find(dependencies.begin(), dependencies.end(), match[1]) == dependencies.end()){
+                        dependencies.push_back(match[1]);
+                    }
+                }
+            } 
+        }
+
+        file.close();
+
+    }
+
+    // Recursion
+    int size = dependencies.size();
+    for(int i = 0; i < size; ++i){
+        for(auto newDepenency: checkFile(dependencies[i])){
+            if(std::find(dependencies.begin(), dependencies.end(), newDepenency) == dependencies.end()){
+                dependencies.push_back(newDepenency);
+            }
+        }
+    }
+    return dependencies;
+}
+
+bool fileExists(std::string fileName){
+    std::ifstream file(fileName);
+    bool exists = false;
+    if(file.is_open()){
+        exists = true;
+    }
+    file.close();
+    return exists;
+}
+
+std::string findSourceFile(std::string headerFileName){
+    std::string header_extensions[] = {".h", ".hh", ".hpp"};
+    std::string source_extensions[] = {".cpp", ".cc", ".C", ".cxx", ".cppm"};
+
+    std::string raw_name;
+
+    // Get the raw name of the header file without its extension
+    for(int i = 0; i < 3; ++i){
+        size_t found = headerFileName.find(header_extensions[i]);
+        if(found != std::string::npos){
+            raw_name = headerFileName.erase(found, header_extensions[i].length());
+            break;
+        }
+    }
+
+    // See if the source file exists
+    for(int i = 0; i < 5; ++i){
+        std::string source_file_name = raw_name + source_extensions[i];
+        if(fileExists(source_file_name)){
+            return source_file_name;
+        }
+    }
+    return "";
+}
+
+int main(int argc, char** argv){
+    std::string fileName;
+    std::string outputName = "a.out";
+    std::vector<std::string> existingDependencyFiles; // The dependencies that will have to be in the compile command
+
+    if(argc < 2){
+        std::cout << "Useage: " << argv[0] << " ./MainFile [-o OutputName]" << std::endl;
+        return 0;
+    }
+
+    fileName = argv[1];
+
+    for(int i = 0; i < argc; ++i){
+        if(strcmp(argv[i], "-o") == 0){
+            outputName = argv[i+1];
+        }
+    }
+
+    std::vector<std::string> dependencies = checkFile(fileName);
+
+    for(auto dependency: dependencies){
+        std::string source_file = findSourceFile(dependency);
+        if(source_file.length() > 0){
+            existingDependencyFiles.push_back(source_file);
+        } 
+    }
+
+    std::string command = "g++ " + fileName;
+    for(auto dependency: existingDependencyFiles){
+        command += " " + dependency;
+    }
+    command += " -o " + outputName;
+
+    system(command.c_str());
+}
---
+++ readme.md
@@ -0,0 +1,35 @@
+# ccbuild
+***
+## Description
+This is a tool for building small to medium sized c++
+Projects that use multiple files.
+
+## Usage
+The tool can be used with the following command:
+```bash
+ccbuild ./myMainFile.cpp -o outputName
+```
+
+## How it works
+The tool opens the main c++ file and looks 
+for ```#include "myHeader.h"``` statements. It supports
+header files with these extensions:
+[ .h  .hh  .hpp ].
+When it has found a header file, it repeats the process recursively until 
+all included files are found.
+Then is begins a search and tries to find the right source files.
+The tool does this by matching the header file name against variations
+of it with the following extensions:
+[ .cpp  .cc  .C  .cxx  .cppm ]
+When it has found the source files, it executes the standard g++
+build command with the appropirate arguments.
+
+## Instalation
+
+Make sure you have g++ installed.
+
+To install the tool, download the ccbuild executable or compile it yourself.
+Then move it to a folder in your computer and create register the PATH
+to it in your Environment Variables. 
+
+Voila! You can now use the tool.
